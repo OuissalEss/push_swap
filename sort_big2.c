3e0bfd2acdb16ffb4237215c9e9cd7a64e2c7852
Ouissal Essamdi <oessamdi@e2r10p9.1337.ma>
2022-04-23 18:18:30 +0000

    updates

--- sort_big2.c
+++ sort_big2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort_big.c                                         :+:      :+:    :+:   */
+/*   sort_big2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oessamdi <oessamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 16:22:11 by oessamdi          #+#    #+#             */
-/*   Updated: 2022/04/23 18:08:48 by oessamdi         ###   ########.fr       */
+/*   Updated: 2022/04/23 18:07:26 by oessamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,35 @@ static void	push_b(t_list **stack_a, t_list **stack_b, t_list *top)
 	int		middle;
 	t_list	*sa_last;
 	t_list	*sb_last;
+	t_list	*head;
+	t_list	*headb;
 
 	middle = lstsize(*stack_b) / 2;
-	sb_last = lstlast(*stack_b);
-	sa_last = lstlast(*stack_a);
+
+		sb_last = lstlast(*stack_b);
+		sa_last = lstlast(*stack_a);
 	if (top->order != 0 && top->nb <= sa_last->nb)
-		sort_big(stack_a, stack_b);
+		sort_big2(stack_a, stack_b);
 	while ((*stack_a)->order != 0)
 	{
 		sb_last = lstlast(*stack_b);
 		sa_last = lstlast(*stack_a);
+		head = *stack_a;
+		headb = *stack_b;
+		printf("\n");
+		while(head)
+		{
+			printf("stack a => nb : %d order : %d\n", head->nb, head->order);
+			head= head->next;
+		}
+		printf("\n");
+		while(headb)
+		{
+			printf("stack b => nb : %d order : %d\n", headb->nb, headb->order);
+			headb= headb->next;
+		}
+		//printf("(*stack_b)->order : %d sa_last->order + 1 : %d\n", (*stack_b)->order, sa_last->order + 1);
+		printf("++++++++++++\n\n");
 		if (((*stack_a)->order == sa_last->order + 1) || ((*stack_a)->order == 0))
 			rotatea(stack_a, 1);
 		else if (((*stack_a)->order == (*stack_a)->next->order + 1) 
@@ -42,14 +61,31 @@ static void	push_a(t_list **stack_a, t_list **stack_b)
 	int		middle;
 	t_list	*sa_last;
 	t_list	*sb_last;
+	t_list	*head;
+	t_list	*headb;
 
 	middle = lstsize(*stack_a) / 2;
 	while (*stack_b)
 	{
 		sb_last = lstlast(*stack_b);
 		sa_last = lstlast(*stack_a);
-		if ((((*stack_a)->order == sa_last->order + 1)
-			|| ((*stack_a)->order == 0)) && (stack_sorted(*stack_a) != 1))
+		printf("middle %d\n", middle);
+		head = *stack_a;
+		headb = *stack_b;
+		printf("\n");
+		while(head)
+		{
+			printf("stack a => nb : %d order : %d\n", head->nb, head->order);
+			head= head->next;
+		}printf("\n");
+		while(headb)
+		{
+			printf("stack b => nb : %d order : %d\n", headb->nb, headb->order);
+			headb= headb->next;
+		}
+		printf("(*stack_b)->order : %d sa_last->order + 1 : %d\n", (*stack_b)->order, sa_last->order + 1);
+		printf("************\n\n");
+		if ((((*stack_a)->order == sa_last->order + 1) || ((*stack_a)->order == 0)) && (stack_sorted(*stack_a) != 1))
 			rotatea(stack_a, 1);
 		else if ((*stack_b)->order >= middle)
 			pusha(stack_b, stack_a);
@@ -76,12 +112,22 @@ static void	sort_a(t_list **stack_a)
 {
 	int		middle;
 	t_list	*sa_last;
+	t_list	*head;
 
 	middle = lstsize(*stack_a) / 2;
 	sa_last = lstlast(*stack_a);
-	while ((((*stack_a)->order == sa_last->order + 1) || ((*stack_a)->order == 0)
-			|| ((*stack_a)->next->order == sa_last->order + 1)) && (stack_sorted(*stack_a) != 1))
+	printf("sorted = %d\n", stack_sorted(*stack_a));
+	while ( (((*stack_a)->order == sa_last->order + 1) || ((*stack_a)->order == 0) || ((*stack_a)->next->order == sa_last->order + 1)) && (stack_sorted(*stack_a) != 1))
 	{
+		printf("middle %d\n", middle);
+		head = *stack_a;
+		printf("\n");
+		while(head)
+		{
+			printf("stack a => nb : %d order : %d\n", head->nb, head->order);
+			head= head->next;
+		}
+		printf("----------\n\n");
 		if ((*stack_a)->next->order == sa_last->order + 1)
 			swapa(stack_a, 1);
 		else
@@ -92,7 +138,7 @@ static void	sort_a(t_list **stack_a)
 	
 }
 
-int	sort_big(t_list **stack_a, t_list **stack_b)
+int	sort_big2(t_list **stack_a, t_list **stack_b)
 {
 	int		min;
 	int		middle;
@@ -104,6 +150,7 @@ int	sort_big(t_list **stack_a, t_list **stack_b)
 	{
 		if ((*stack_a)->order < middle)
 		{
+			printf("push b : %d\n", (*stack_a)->nb);
 			pushb(stack_a, stack_b);
 			min++;
 		}
@@ -112,6 +159,7 @@ int	sort_big(t_list **stack_a, t_list **stack_b)
 	}
 	while (stack_sorted(*stack_a) != 1 || *stack_b != NULL)
 	{
+		//printf("hello\n");
 		top = (*stack_a);
 		push_a(stack_a, stack_b);
 		sort_a(stack_a);
